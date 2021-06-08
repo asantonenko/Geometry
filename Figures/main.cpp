585b538d6167ce5486ac67f79b743eb1048829d7
Oleksandr Antonenko <asantonenko@gmail.com>
2021-06-08 18:27:46 +0300

    Russian comments

--- Figures/main.cpp
+++ Figures/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    //ƒанна€ строка инициализирует генератор случайных чисел
     srand(time(NULL));
 	/*vector<Triangle> figures;
     int number;
@@ -23,24 +24,25 @@ int main(int argc, char **argv)
         if (c<=abs(a-b)) c=abs(a-b)+1;
         figures.push_back(Triangle(a,b,c));
     }
-    
+
     for (size_t i=0; i<figures.size(); i++)
     {
         cout <<figures[i] <<endl;
     }*/
-      
+
     /*Square sq(5);
     cout <<sq <<endl;
     */
     /*Figure * f = new Triangle (3,4,5);
     cout <<f->area() <<endl;*/
-    
+
+    //¬ектор с фигурами
     vector<Figure*> figures;
-    
+
     int number;
     cout <<"Enter number of figures: ";
     cin >>number;
-    
+
     for (int i = 0; i < number; i++)
     {
         switch (rand()%2)
@@ -58,20 +60,20 @@ int main(int argc, char **argv)
             figures.push_back(new Square(rand()%4+1));
         }
     }
-    
+
     for (size_t i=0; i<figures.size(); i++)
     {
         cout <<*(figures[i]) <<endl;
     }
-    
+
     double total=0;
     for (int i = 0; i < figures.size(); i++)
     {
         total+=figures[i]->area();
     }
     cout <<total <<endl;
-        
+
     //vector <Figure> sq;
-    
+
 	return 0;
 }
